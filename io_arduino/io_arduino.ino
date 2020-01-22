a59381bf440fc3f2fde6f211e46fd904fe4cd3e3
Phil Dougherty <phildo211@gmail.com>
2020-01-22 17:53:48 -0600

    mic experimentation

--- io_arduino/io_arduino.ino
+++ io_arduino/io_arduino.ino
@@ -9,6 +9,8 @@
   //btns
 #define BTN_A_PIN 8
 #define BTN_B_PIN 9
+  //mic
+#define MIC_PIN 10
 
 //strip constants
 #define STRIP_NUM_LEDS 16
@@ -27,6 +29,9 @@ int btn_a_delta;
 unsigned int btn_a_down;
 int btn_b_delta;
 unsigned int btn_b_down;
+
+//mic
+int mic_hot;
 
 void setup()
 {
@@ -50,17 +55,24 @@ void setup()
   btn_a_down = 0;
   btn_b_delta = 0;
   btn_b_down = 0;
+
+  //mic
+  pinMode(MIC_PIN,INPUT);
+  digitalWrite(MIC_PIN,0);
+  mic_hot = 0;
 }
 
 void loop()
 {
+  mic_hot = digitalRead(MIC_PIN);
+
   btn_a_delta = 0;
-  if(!digitalRead(BTN_A_PIN)) { if(!btn_a_down) btn_a_delta =  1; btn_a_down = 1; }
-  else                        { if(btn_a_down)  btn_a_delta = -1; btn_a_down = 0; }
+  if(mic_hot || !digitalRead(BTN_A_PIN)) { if(!btn_a_down) btn_a_delta =  1; btn_a_down = 1; }
+  else                                   { if(btn_a_down)  btn_a_delta = -1; btn_a_down = 0; }
 
   btn_b_delta = 0;
-  if(!digitalRead(BTN_B_PIN)) { if(!btn_b_down) btn_b_delta =  1; btn_b_down = 1; }
-  else                        { if(btn_b_down)  btn_b_delta = -1; btn_b_down = 0; }
+  if(mic_hot || !digitalRead(BTN_B_PIN)) { if(!btn_b_down) btn_b_delta =  1; btn_b_down = 1; }
+  else                                   { if(btn_b_down)  btn_b_delta = -1; btn_b_down = 0; }
 
   byte msg = 0x00;
   msg |= (btn_a_delta & 0x0F);
