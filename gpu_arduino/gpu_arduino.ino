d89a85fb87c732efda2cb5420e3824a42287d69e
Phil Dougherty <phildo211@gmail.com>
2020-02-04 14:52:56 -0600

    attempt at gpu RLE

--- gpu_arduino/gpu_arduino.ino
+++ gpu_arduino/gpu_arduino.ino
@@ -1,6 +1,11 @@
 #include <FastLED.h>
 
+//enum
+#define CMD_WHORU '0'
+#define CMD_DATA '1'
+
 //customize
+#define AID "GPU"
 #define STRIP_BRIGHTNESS 100 //0-255
 #define STRIP_MAX_BRIGHTNESS 256
 #define STRIP_MIN_BRIGHTNESS 0
@@ -13,19 +18,19 @@
 #define STRIP_LED_TYPE WS2812
 #define STRIP_COLOR_ORDER RGB
 
-//sync w/ pi
-#define FLUSH_TRIGGER "FLUSH"
+//serial constants (sync w/ pi)
+#define CMD_PREAMBLE "CMD_"
 #define BAUD_RATE 1000000
+#define T_CONSIDERED_DEAD 10000
 
 //strip
 CRGB strip_leds[STRIP_NUM_LEDS];
 CRGB lut[16];
 
-//serial
-int buff_n;
-byte *buff;
-int buff_i;
-int trigger_i;
+//data
+size_t dbuff_s;
+byte *dbuff;
+unsigned int dbuff_i;
 
 CRGB dampen_color(CRGB in, int amt, int maxamt)
 {
@@ -97,13 +102,64 @@ void initlut()
   }
 }
 
-void buffToStrip()
+void dbuffToStrip()
 {
   for(int i = 0; i < STRIP_NUM_LEDS/2; i++)
   {
     strip_leds[i*2  ] = lut[buff[i]&0x0F];
     strip_leds[i*2+1] = lut[buff[i]>>4];
   }
+}
+
+unsigned char serial_spinread(char *c)
+{
+  unsigned int cmd_dead_t = 0;
+  while(cmd_dead_t < T_CONSIDERED_DEAD)
+  {
+    if(Serial.available()) { *c = Serial.read(); return 1; }
+    cmd_dead_t++;
+  }
+  return 0;
+}
+
+void cmd_whoru()
+{
+  Serial.write(AID);
+}
+
+void cmd_data()
+{
+  char d;
+
+  unsigned int dcmd_n = 0;
+  for(int i = 0; i < 3; i++)
+  {
+    if(!serial_spinread(&d)) return;
+    dcmd_n *= 10;
+    dcmd_n += atoi(d);
+  }
+
+  int strip_i = 0;
+  while(dcmd_n) //perform commands
+  {
+    if(!serial_spinread(&d)) return;
+    char n = d;
+    if(!serial_spinread(&d)) return;
+    char r = d;
+    if(!serial_spinread(&d)) return;
+    char g = d;
+    if(!serial_spinread(&d)) return;
+    char b = d;
+    CRGB color = CRGB(r,g,b);
+    while(n)
+    {
+      strip_leds[strip_i++] = color;
+      n--;
+    }
+    dcmd_n--;
+  }
+
+  FastLED.show();
 }
 
 void setup()
@@ -114,13 +170,10 @@ void setup()
 
   initlut();
 
-  buff_n = STRIP_NUM_LEDS/2+strlen(FLUSH_TRIGGER);
-  buff = (byte *)malloc(sizeof(byte)*buff_n+1);
-  memset(buff,0,sizeof(byte)*buff_n+1);
-  strcpy((char *)buff+(buff_n-strlen(FLUSH_TRIGGER)),FLUSH_TRIGGER); //actually unnecessary
-
-  buff_i = 0;
-  trigger_i = 0;
+  dbuff_s = STRIP_NUM_LEDS/2+strlen(FLUSH_TRIGGER);
+  buff = (byte *)malloc(sizeof(byte)*dbuff_s+1);
+  memset(buff,0,sizeof(byte)*dbuff_s+1);
+  strcpy((char *)buff+(dbuff_s-strlen(FLUSH_TRIGGER)),FLUSH_TRIGGER); //actually unnecessary
 
   for(int i = 0; i < STRIP_NUM_LEDS; i++) strip_leds[i] = lut[0];
 
@@ -132,27 +185,19 @@ void setup()
 
 void loop()
 {
-  while(Serial.available() > 0)
+  char d;
+  //get preamble
+  for(int i = 0; i < strlen(CMD_PREAMBLE))
   {
-    buff[buff_i++] = Serial.read();
-    if(trigger_i && buff[buff_i-1] == FLUSH_TRIGGER[trigger_i])
-    {
-      trigger_i++;
-      if(trigger_i == strlen(FLUSH_TRIGGER))
-      {
-        if(buff_i == buff_n)
-        {
-          buffToStrip();
-          FastLED.show();
-        }
-        buff_i = 0;
-        trigger_i = 0;
-      }
-    }
-    else if(buff[buff_i-1] == FLUSH_TRIGGER[0]) trigger_i = 1;
-    else                                        trigger_i = 0;
-
-    if(buff_i == buff_n) buff_i = 0;
+    if(!serial_spinread(&d)) return;
+    if(d != CMD_PREAMBLE[cmd_pre_i]) return;
+  }
+  //run command
+  if(!serial_spinread(&d)) return;
+  switch(d)
+  {
+    case CMD_WHORU: cmd_whoru(); break;
+    case CMD_DATA:  cmd_data(); break;
   }
 }
 
